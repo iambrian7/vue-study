8cec261dc42d138ab726e37e73c6ed41ba1295be
iambrian7 <bkcgee@gmail.com>
2017-08-22 08:27:25 -0500

    new files added

---
+++ components/first-component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="first-component">
+  <h1>{{ msg}}</h1>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'first-component',
+  data () {
+    return {
+      msg: 'Welcome to Your first component'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.class {
+   
+    background-color: #cccccc;
+    width: 100wh;
+    height: 100vh;
+}
+
+</style>
